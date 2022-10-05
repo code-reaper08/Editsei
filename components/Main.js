64230c92d3704c7063f1e0c3716c1417f9fad9af
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-10-06 01:39:53 +0530

    fix: add deafult bundle

--- components/Main.js
+++ components/Main.js
@@ -16,6 +16,11 @@ export default function Main() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
+      let Def_Bundle = {
+        md_raw: "# Hello",
+        clean_html: "<h1>Hello</h1>",
+      };
+      window.localStorage.setItem("latest-content", JSON.stringify(Def_Bundle));
       let Bundle = JSON.parse(window.localStorage.getItem("latest-content"));
       setTextcontent(Bundle.md_raw ? Bundle.md_raw : "# Hello");
       setParsedcontent(Bundle.clean_html ? Bundle.clean_html : "Hello");
