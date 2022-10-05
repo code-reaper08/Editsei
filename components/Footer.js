fc23da13a5b5a2e7bcede5124f05d54a4151b03a
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-10-06 00:32:22 +0530

    feat: add footer

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-row justify-center items-center">
+      <div className="text-white font-mono text-lg">
+        Made with [love] with{" "}
+        <a className="text-yellow-200" href="https://github.com/code-reaper08">
+          Vishwa R
+        </a>
+      </div>
+    </footer>
+  );
+}
