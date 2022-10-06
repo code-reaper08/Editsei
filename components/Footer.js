59440352eeb638ac42794658d8ea0b4d2d4dc336
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-10-06 14:28:59 +0530

    fix: typo in footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-row justify-center items-center">
       <div className="text-white font-mono text-lg flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center">
-        <div className="mr-2">Made with [love] with </div>
+        <div className="mr-2">Made with [love] by </div>
         <a className="text-yellow-200" href="https://github.com/code-reaper08">
           Vishwa R
         </a>
