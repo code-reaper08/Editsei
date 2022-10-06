1cec9f2743935f7796e29e7d4d070f43d48024f1
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-10-06 12:09:33 +0530

    fix: responsive footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="flex flex-row justify-center items-center">
-      <div className="text-white font-mono text-lg">
-        Made with [love] with{" "}
+      <div className="text-white font-mono text-lg flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center">
+        <div className="mr-2">Made with [love] with </div>
         <a className="text-yellow-200" href="https://github.com/code-reaper08">
           Vishwa R
         </a>
