4cb71aa3220ddfcabe40602c3cc525b504c2aefe
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-10-07 22:41:02 +0530

    fix: remove mis-render of music component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Head from "next/head";
+import Music from "../components/Music";
 
 export default function Home() {
   return (
